1251fe59130a49622318e6189b204c98ba63b72c
伊東和成 <itokazunari5375@icloud.com>
2021-07-19 18:19:36 +0900

    storybookの導入

---
+++ project-story/components/atoms/Button.tsx
@@ -0,0 +1,30 @@
+interface Props {
+  purpose?: string;
+}
+
+export const Button: React.FC<Props> = ({ purpose = "primary", children }) => {
+  return (
+    <button className={`button ${purpose}`}>
+      {children}
+      <style jsx>{`
+        .button {
+          width: 100px;
+          padding: 4px 8px;
+          border: none;
+          border-radius: 4px;
+          font-size: 16px;
+        }
+
+        .primary {
+          background-color: #007bff;
+          color: #fff;
+        }
+
+        .danger {
+          background-color: #dc3545;
+          color: #fff;
+        }
+      `}</style>
+    </button>
+  );
+};
